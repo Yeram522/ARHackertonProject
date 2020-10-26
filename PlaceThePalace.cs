55bd8bc2687745dfaa493d8536927388df020621
Yeram522 <kyeram522@gmail.com>
2020-10-27 02:51:11 +0900

    1. Get the var from PlaceThePlace Script. 2. Made function SpawnpositionCalculater/ SpawnGhost 3. BUT didn't Bild Test ^^

--- PlaceThePalace.cs
+++ PlaceThePalace.cs
@@ -8,13 +8,14 @@ public class PlaceThePalace : MonoBehaviour
 {
     public ARRaycastManager arRaycaster;
     public GameObject spawnPrefab;
-
+    public Pose m_spawnprefabPose;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    private int hitCount = 0;
+    public bool GhostTrigger=false;
+
     void Update()
     {
 
-        if(Input.touchCount==0 | hitCount>=1)
+        if(Input.touchCount==0 | GhostTrigger == true)
         {
             return;
         } 
@@ -28,10 +29,17 @@ public class PlaceThePalace : MonoBehaviour
 
         if (arRaycaster.Raycast(touch.position, hits, TrackableType.Planes)) {
            Pose hitPose = hits[0].pose;
-            hitCount += 1;
+            m_spawnprefabPose= hits[0].pose; //유령생성 위치의 기준점 pose
+            GhostTrigger = true;
+
 
             Instantiate(spawnPrefab, hitPose.position, hitPose.rotation);
+
+            //transform.position = hitPose.position;
+            //transform.rotation = hitPose.rotation;
+            //GhostList[0].transform.position = hitPose.position;
         };
 
+        
     }
 }
